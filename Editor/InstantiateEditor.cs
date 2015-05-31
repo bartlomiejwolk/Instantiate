7a8c7e2cfef1d8bb818377e44a9ce2b75c114f9f
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-31 17:17:03 +0200

    Initial commit

---
+++ Editor/InstantiateEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+
+namespace InstantiateEx {
+
+    [CustomEditor(typeof(Instantiate))]
+    [CanEditMultipleObjects]
+    public sealed class InstantiateEditor : Editor {
+        #region FIELDS
+
+        private Instantiate Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (Instantiate)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    InstantiateEx.Instantiate.Version,
+                    InstantiateEx.Instantiate.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/Instantiate")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(Instantiate));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
---
+++ Instantiate.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace InstantiateEx {
+
+    public sealed class Instantiate : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "Instantiate";
+
+        #endregion CONSTANTS
+
+        #region DELEGATES
+        #endregion DELEGATES
+
+        #region EVENTS
+        #endregion EVENTS
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "Instantiate";
+#pragma warning restore0414
+
+        #endregion FIELDS
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        #endregion INSPECTOR FIELDS
+
+        #region PROPERTIES
+
+        /// <summary>
+        ///     Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion PROPERTIES
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        #endregion UNITY MESSAGES
+
+        #region EVENT INVOCATORS
+        #endregion INVOCATORS
+
+        #region EVENT HANDLERS
+        #endregion EVENT HANDLERS
+
+        #region METHODS
+        #endregion METHODS
+
+    }
+
+}
